7206283e6bb0b59c452cd3bd5573d06cf3c461ab
ComeToChanka <37425508+username12345679@users.noreply.github.com>
2018-07-11 22:56:15 -0400

    Question 2 Make Triangle Solution Code

---
+++ MakeTriangle.java
@@ -0,0 +1,11 @@
+public class MakeTriangle {
+
+	public static void main(String[] args) {
+		String triangle = "";
+		for (int i = 1; i < 5; i++) {
+			triangle = triangle + "X";
+			System.out.println(triangle);
+		}
+
+	}
+}
