7c8fe20ba15cfedea93660edeb43ad88d7ad2dad
ComeToChanka <37425508+username12345679@users.noreply.github.com>
2018-07-11 22:09:18 -0400

    answer to question 1

--- MakeSquare.java
+++ MakeSquare.java
@@ -3,7 +3,7 @@ public class MakeSquare
 	public static void main(String[] args) 
 	{
 // for (variable initialized; continue if boolean is true; execute this code at the end of this iteration)
-		for(int i = 0; i < 3; i++)
+		for(int i = 0; i < 4; i++)
 		{
 			System.out.println("XXX");
 		}
