1557429b0a884a1b3db1d6ed685c410933be3858
ComeToChanka <37425508+username12345679@users.noreply.github.com>
2018-07-11 21:36:09 -0400

    question 1 solution

---
+++ MakeSquare.java
@@ -0,0 +1,13 @@
+public class MakeSquare 
+{
+	public static void main(String[] args) 
+	{
+// for (variable initialized; continue if boolean is true; execute this code at the end of this iteration)
+		for(int i = 0; i < 3; i++)
+		{
+			System.out.println("XXX");
+		}
+
+		
+	}
+}
